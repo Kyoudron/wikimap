dfa6c2136fbef7ef53256f82497ae16cad7f6dae
Kapish <kapish.k.1@gmail.com>
2017-02-03 14:53:44 -0500

    access to database

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,24 +1,36 @@
 $(() => {
+
+
+// let $mapTitle = createMapElement(data)
+
   $.ajax({
     method: "GET",
     url: "/api/maps"
   }).done((maps) => {
+    html = '';
     for(map of maps) {
-      $("<div>").text(map.title).appendTo($("body"));
+      html += createMapElement(map);
     }
+    $('.map-list').html(html);
   });;
 
+  function createMapElement(map){
+    let title = map.title;
 
+    let html = `
+            <div class="col-md-4 portfolio-item">
+                <a href="#">
+                    <img class="img-responsive" src="http://placehold.it/700x400" alt="">
+                </a>
+                <h3>
+                    <a href="#">${title}</a>
+                </h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+            </div>`
 
-// posts the user names
-  //  $.ajax({
-  //   method: "GET",
-  //   url: "/api/users"
-  // }).done((users) => {
-  //   for(user of users) {
-  //     $("<div>").text(user.name).appendTo($("body"));
-  //   }
-  // });;
+    return html;
+  }
+
 })
 
 
