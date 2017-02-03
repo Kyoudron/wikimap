aa0ef9c13c2017c3016d3c1d3940c0eb9b54dcf9
Kapish <kapish.k.1@gmail.com>
2017-02-03 11:48:55 -0500

    able to connect to database

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,34 +1,25 @@
 $(() => {
   $.ajax({
     method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
+    url: "/api/maps"
+  }).done((maps) => {
+    for(map of maps) {
+      $("<div>").text(map.title).appendTo($("body"));
     }
   });;
 
-function renderMap (data) {
-  $container = $('#map')
-  data.forEach(function(map){
-    let $mapTitle = createMap(map)
-    $container.prepend($mapTitle)
-  })
-}
 
 
-
-
-// For kapish to referene
-// <section id="map">
-
-// <body class="allmaps">
-//     <a href="<%= `/view/${maptitle}` %>"> ${NameOfMap}</a>
-//   </body>
-
-// </section>
-
-});
+// posts the user names
+  //  $.ajax({
+  //   method: "GET",
+  //   url: "/api/users"
+  // }).done((users) => {
+  //   for(user of users) {
+  //     $("<div>").text(user.name).appendTo($("body"));
+  //   }
+  // });;
+})
 
 
 
