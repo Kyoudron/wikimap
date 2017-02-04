c770464de79b37ee4645bd48e527dbee15ae6754
clairellesage <clairellesage@gmail.com>
2017-02-04 23:12:30 +0000

    login and logout

---
+++ routes/checkIfLoggedIn.js
@@ -0,0 +1,16 @@
+// "use strict";
+
+// const express = require('express');
+// const router  = express.Router();
+// const bodyParser  = require("body-parser");
+
+// module.exports = (req, res) => {
+//   if (!req.session.user_id) {
+//     res.status(401);
+//     res.redirect("/login");
+//     return;
+//   } else {
+//   let session = req.session.user_id;
+//   }
+// }
+
