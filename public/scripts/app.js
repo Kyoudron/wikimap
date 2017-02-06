6672ec6e7e9664f8fdf3ba74217405d01595ff24
clairellesage <clairellesage@gmail.com>
2017-02-06 14:51:18 +0000

    markers are all working, but no maps on profile

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -42,27 +42,101 @@ $.ajax({
     })
     .done((markers) => {
     html = '';
-    for(let marker of markers) {
-      html += markerMapElement(marker);
-    }
+    markerMapElement(markers)
     $('.maplatlng').html(html);
   });
 
 
 
-function markerMapElement(marker){
-    let lat = marker.latitude;
-    let lng = marker.longitude;
+function markerMapElement(markers){
+console.log(markers)
+    var mapOptions = {
+      zoom: 12,
+      center: {lat: 43.6532, lng: -79.3832},
+      disableDoubleClickZoom: true
+    }
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-    let html = `
-                <h3>
-                    ${lat} ${lng}
-                </h3>
-          `
-    return html;
+    let infoWindow = new google.maps.InfoWindow({map: map
+    });
+
+    for (i =0; i < markers.length; i++) {
+      let lat = markers[i].latitude;
+      let lng = markers[i].longitude;
+      let myLatLng = new google.maps.LatLng(lat, lng);
+      var marker = new google.maps.Marker({
+      position: myLatLng,
+      });
+      marker.setMap(map)
+    }
+
+// var marker2 = new google.maps.Marker({
+//       position :{lat: 43.6532, lng: -79.3832}
+//   });
+//   marker2.setMap(map)
+
+
+
+if (navigator.geolocation) {
+navigator.geolocation.getCurrentPosition(function(position) {
+  let pos = {
+    lat: position.coords.latitude,
+    lng: position.coords.longitude
+  };
+  infoWindow.setPosition(pos);
+  infoWindow.setContent('<b>You are here.</b>');
+  map.setCenter(pos);
+
+  }, function() {
+    handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    handleLocationError(false, infoWindow, map.getCenter());
   }
 
+// marker generating event listener on double click
 
+  map.addListener('dblclick', function(event) {
+    function addMarker(location) {
+      let marker = new google.maps.Marker({
+        position: location,
+        map: map
+      })
+
+      marker.addListener('click', function(event) {
+
+        let userHTML = "<table>" +
+               "<tr><td>Title:</td> <td><input type='text' id='title'/> </td> </tr>" +
+               "<tr><td>Description:</td> <td><input type='text' id='description'/></td> </tr>" +
+               "<tr><td>Image:</td> <td><input type='file' id='image'/></td> </tr>" +
+               "<tr><td></td><td><input class='markerSubmit' type='button' value='Save & Close'/></td></tr>" + "</table>";
+
+        let userInfoWindow = new google.maps.InfoWindow({
+          content: userHTML
+        })
+
+        userInfoWindow.open(map, marker);
+
+        $(".markerSubmit").click(() => saveData(location));
+        $(".markerSubmit").click(() => userInfoWindow.close());
+      })
+    }
+    addMarker(event.latLng);
+  });
+    let html = `
+                  <h3>
+                      ${lat} ${lng}
+                  </h3>
+              `
+    return html;
+
+}
+
+
+
+
+
+// favorites
   function faveMapElement(map){
     let title = map.title;
     let id = map.id;
